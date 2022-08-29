9132721b8998c0a5ec529e90f8172457cff387c3
eslamabdallah74 <eslamabdallah301@gmail.com>
2022-08-29 07:42:59 +0200

    setup tailwindcss

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-center">
+        <h1 class="text-3xl font-bold underline bg-gray-500">
+            Hello world!
+        </h1>
+        <h1 class="text-3xl font-bold text-white underline bg-gray-900">
+            Hello world!
+        </h1>
+        <h2 class="text-6xl text-green-800"> common man</h2>
+        <h2 class="text-6xl text-red-800"> common man</h2>
+        <h4 class="font-bold text-9xl">ffffff</h4>
+        <h5 class="bg-red-200">bacsdadsa</h5>
+    </div>
+</template>
+  
