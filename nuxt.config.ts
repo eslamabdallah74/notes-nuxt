a9e1975cd0d0b3cd653af453cdda9e38f799c45f
eslamabdallah74 <eslamabdallah301@gmail.com>
2022-09-01 10:18:05 +0200

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,18 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ["~/assets/css/tailwind.css"],
-    build: {
-      postcss: {
-        postcssOptions: {
-          plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-          },
+  router: {
+    base: '/<notes-nuxt>/'
+  },
+  css: ["~/assets/css/tailwind.css"],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
         },
       },
     },
+  },
 })
